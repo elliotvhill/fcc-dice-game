99694e00dea55ee8517fef2f02e446be1f649e32
Elliot <ellthrowaway2@icloud.com>
2024-07-15 14:34:24 -0400

    initial variables creation

---
+++ script.js
@@ -0,0 +1,18 @@
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const currentRound = document.getElementById("current-round");
+const currentRoundRolls = document.getElementById("current-round-rolls");
+const totalScore = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesBtn = document.getElementById("rules-btn");
+const rulesContainer = document.querySelector(".rules-container");
+
+let isModalShowing = false;
+let diceValuesArr = [];
+let rolls = 0;
+let score = 0;
+let total = 0;
+let round = 1;
